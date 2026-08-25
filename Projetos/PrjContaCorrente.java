fafe6c5b720ee36feabf5c6087191f7271e53113
douglaswenzel <douglas.wenzel@hotmail.com>
2026-08-25 17:24:16 -0300

    feat(ContaCorrente): Adição dos métodos getters e esetters para numero e saldo

---
+++ Projetos/PrjContaCorrente.java
@@ -0,0 +1,29 @@
+package prjcontacorrente;
+
+/**
+ *
+ * @author Douglas Wenzel
+ */
+
+public class PrjContaCorrente {
+
+    private int numero;
+    private double saldo;
+    
+    public void setNumero(int numero) {
+      this.numero = numero;
+    }
+    
+    public int getNumero() {
+        return(numero);
+    }
+    
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+    
+    public double getSaldo() {
+        return(saldo);
+    }
+    
+}
