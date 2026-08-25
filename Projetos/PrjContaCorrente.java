242e9d578046ac8b5c497f6233225713b6fb333d
douglaswenzel <douglas.wenzel@hotmail.com>
2026-08-25 18:22:56 -0300

    feat: Criação do menu de chamadas e funções de operações de deposito e saque

--- Projetos/PrjContaCorrente.java
+++ Projetos/PrjContaCorrente.java
@@ -6,24 +6,32 @@ package prjcontacorrente;
  */
 
 public class PrjContaCorrente {
-
-    private int numero;
-    private double saldo;
-    
-    public void setNumero(int numero) {
-      this.numero = numero;
-    }
-    
-    public int getNumero() {
-        return(numero);
-    }
-    
-    public void setSaldo(double saldo){
+            private int numero;
+            private double saldo;
+            
+    public PrjContaCorrente(int numero, double saldo) {
+        this.numero = numero;
         this.saldo = saldo;
     }
-    
-    public double getSaldo() {
-        return(saldo);
+
+        public int getNumero() {
+            return(numero);
+        }
+
+        public void depositar(double valor){
+            if (saldo <= 0) {
+                System.out.println("O saldo deve ser maior que zero. Busca por linhas de crédito? Consulte um de nossos especialistas, rs!");
+            } else {
+                this.saldo = valor;
+                System.out.println("Valor sacado com sucesso! Seu saldo atual é de: R$" + this.saldo);
+            }
+        }
+        
+        public double getSaldo() {
+            return(saldo);
+        }
+        
+        
     }
     
 }
