75468ebb16d1f0e467a15bb8406d77e1b57764a7
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2026-09-08 11:04:40 -0300

    fix(aplic): Ajustado a chama dos métodos '

--- Projetos/PrjExemploGenerEspecPolimorfismo/src/Aplic.java
+++ Projetos/PrjExemploGenerEspecPolimorfismo/src/Aplic.java
@@ -1,5 +1,6 @@
 
 import fatec.poo.model.FuncionarioHorista;
+import fatec.poo.model.FuncionarioMensalista;
 
 public class Aplic {
     
@@ -9,10 +10,24 @@ public class Aplic {
         
         funcHor.setQtdHroraTrab(120);
         
+        System.out.println("Funcionário: " + funcHor.getNome());
         System.out.println("Salário Bruto => " + funcHor.calcSalBruto());
         System.out.println("Descontos => " + funcHor.calcDesconto());
         System.out.println("Salário Líquido => " + funcHor.calcSalLiquido());
-               
+        System.out.println("Data de admissão: " + funcHor.getDtAdmissao());
+        
+        System.out.println("===================================");
+        
+        FuncionarioMensalista funcMen = new FuncionarioMensalista(1034, "Matheus Pierre", "12/05/2025",1820);
+        
+        funcMen.setNumSalMin(1);
+        
+        System.out.println("Funcionário: " + funcMen.getNome());
+        System.out.println("Salário Bruto MENSALISTA => " + funcMen.calcSalBruto() );
+        System.out.println("Descontos => " + funcMen.calcDesconto());
+        System.out.println("Salário Líquido => " + funcMen.calcSalLiquido());
+        System.out.println("Data de admissão: " + funcMen.getDtAdmissao());
+        
     }
     
 }
